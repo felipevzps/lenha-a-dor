63fb60ee825a75523b7550415225b9a25aa638cb
Felipe Vaz Peres <felipe.vzps@gmail.com>
2023-12-15 15:48:10 -0300

    added locateOnScreen

---
+++ locateOnScreen.py
@@ -0,0 +1,9 @@
+import pyautogui
+from time import sleep
+
+#pyautogui.PAUSE = 0.01 # pyautogui delay default = 0.1
+
+# Function to locate the (left, top) position of each image
+while True:
+    # Mouse position
+    print(pyautogui.position())
